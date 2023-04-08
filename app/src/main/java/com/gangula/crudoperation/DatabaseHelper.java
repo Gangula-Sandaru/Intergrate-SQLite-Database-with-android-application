226ea3e372b7db328410134bfa86d287d058b883
Gangula Sandaru Dinusantha <gangula.sandaru@gmail.com>
2023-04-08 20:32:02 +0530

    add veriable, constructor to the file

--- app/src/main/java/com/gangula/crudoperation/DatabaseHelper.java
+++ app/src/main/java/com/gangula/crudoperation/DatabaseHelper.java
@@ -1,4 +1,32 @@
 package com.gangula.crudoperation;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "students.db";
+    public static final String TABLE_NAME= "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "COURSE";
+    public static final String COL_4 = "MARKS";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context,DATABASE_NAME, null, 1);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
