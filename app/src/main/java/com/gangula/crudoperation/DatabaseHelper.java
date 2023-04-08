587f8a533a7449d6f18c60b04f9dd71d009a0fad
Gangula Sandaru Dinusantha <gangula.sandaru@gmail.com>
2023-04-08 20:46:29 +0530

    created an object to the created the databse

--- app/src/main/java/com/gangula/crudoperation/DatabaseHelper.java
+++ app/src/main/java/com/gangula/crudoperation/DatabaseHelper.java
@@ -6,6 +6,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "students.db";
@@ -18,15 +20,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper(@Nullable Context context) {
         super(context,DATABASE_NAME, null, 1);
 
+        // created a constructor  for created the database
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+        // SQL query for the database
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME + "(ID INTEGER primary key autoincrement, " + "NAME TEXT, COURSE TEXT, MARKES INTERGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+
 
     }
 }
